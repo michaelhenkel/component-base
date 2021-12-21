b541973a6831aae237e89e585bd5a259de84bc89
mhenkel <mhenkel@juniper.net>
2021-12-20 17:25:54 -0800

    fix

--- logs/logs.go
+++ logs/logs.go
@@ -32,7 +32,14 @@ const logFlushFreqFlagName = "log-flush-frequency"
 var logFlushFreq = pflag.Duration(logFlushFreqFlagName, 5*time.Second, "Maximum number of seconds between log flushes")
 
 func init() {
-	klog.InitFlags(flag.CommandLine)
+	fs := flag.NewFlagSet("", flag.ContinueOnError)
+	for _, arg := range flag.CommandLine.Args() {
+		if flag.Lookup(arg) != nil {
+			f := flag.Lookup(arg)
+			fs.Set(f.Name, f.Value.String())
+		}
+	}
+	klog.InitFlags(fs)
 }
 
 // AddFlags registers this package's flags on arbitrary FlagSets, such that they point to the
